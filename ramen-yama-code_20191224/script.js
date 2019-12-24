e0da2494035a9ad2a3e386729d933ab467f3d2c2
MASAHIRO OKAMOTO <hisarararu@gmail.com>
2019-12-24 11:50:29 +0900

    Add files via upload

--- ramen-yama-code_20191224/script.js
+++ ramen-yama-code_20191224/script.js
@@ -9,3 +9,21 @@ $(function() {
 	　}
 	　});
 	});
+
+	$(function () {
+    var windowWidth = $(window).width();
+    var htmlStr = $('#pageplugin').html();
+    var timer = null;
+    $(window).on('resize',function() {
+        var resizedWidth = $(window).width();
+        if(windowWidth != resizedWidth && resizedWidth < 500) {
+            clearTimeout(timer);
+            timer = setTimeout(function() {
+                $('#pageplugin').html(htmlStr);
+                window.FB.XFBML.parse();
+　　　　　　　　　　　//window.FB.XFBML.parse()で再レンダリングします。
+                var windowWidth = $(window).width();
+            }, 500);
+        }
+    });
+	});
